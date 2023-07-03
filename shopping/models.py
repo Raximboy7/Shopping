e1096eb09cda8f8253adf045bd6537d8c8423c21
RaximboyIbroximov <raximboy4200@gmail.com>
2023-07-03 20:40:42 +0500

    catigoriya yaratim

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cotegory(models.Model):
+    name = models.TextField(max_length=122)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField()
+    
+    def __str__(self) -> str:
+        return self.name
