93bc0b79827e81317da132ee2a8e002b035511f6
Ulugbek-Mavlonov <126501349+Ulugbek-Mavlonov@users.noreply.github.com>
2023-07-03 21:46:21 +0500

    User imported

--- shopping/models.py
+++ shopping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Cotegory(models.Model):
@@ -19,11 +20,11 @@ class Product(models.Model):
     
 class Order(models.Model):
     user = models.ForeignKey(
-        'auth.User',
+        User,
         on_delete = models.CASCADE,
         related_name='orders',
     )
     title = models.ForeignKey('Product', related_name='order')
     name = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     order_date = models.DateField(auto_now_add=True)
