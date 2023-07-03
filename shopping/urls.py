4651104c55c159af013fee4ffe312b173de3853e
asadbek000002 <asadbektuygunov9@gmail.com>
2023-07-03 20:54:43 +0500

    view tayyor

---
+++ shopping/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import indexView, contactView, aboutView, productsView, singleProductsView
+
+urlpatterns = [
+    path('', indexView, name='home'),
+    path('contact/', contactView, name='contact'),
+    path('about/', aboutView, name='about'),
+    path('products', productsView, name='product'),
+    path('single', singleProductsView, name='single'),
+]
