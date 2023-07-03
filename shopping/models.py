63e4bdf63b652961116efdbcdb8e910693b83378
Ulugbek-Mavlonov <126501349+Ulugbek-Mavlonov@users.noreply.github.com>
2023-07-03 21:20:30 +0500

    Product and Order added in Modul

--- shopping/models.py
+++ shopping/models.py
@@ -8,3 +8,22 @@ class Cotegory(models.Model):
     
     def __str__(self) -> str:
         return self.name
+    
+class Product(models.Model):
+    cotegory = models.ForeignKey('Cotegory' , on_delete= models.CASCADE, related_name='product')
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    
+class Order(models.Model):
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete = models.CASCADE,
+        related_name='orders',
+    )
+    title = models.ForeignKey('Product', related_name='order')
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    order_date = models.DateField(auto_now_add=True)
